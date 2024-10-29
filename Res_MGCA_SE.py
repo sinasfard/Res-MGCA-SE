7f05726aa5485e75a59212a4f58efb412e508068
Sina Soleimani Fard (Sina SFard) <91623372+sinasfard@users.noreply.github.com>
2024-10-29 02:39:39 -0600

    Update Res_MGCA_SE.py

--- Res_MGCA_SE.py
+++ Res_MGCA_SE.py
@@ -1,26 +1,27 @@
 import torch
 import torch.nn as nn
 from torchvision.models import resnet18, resnet50
-from MGCA_SE import *
 
 class Res_MGCA_SE(nn.Module):
-    def __init__(self, backbone):
+    def __init__(self, backbone, num_classes):
         super(Res_MGCA_SE, self).__init__()
 
         if backbone == 'res18':
             self.model = resnet18()
-            model.layer4[1]=MGCA_SE(512,64,8,64,512)
-            model.layer3[1]=MGCA_SE(256,64,8,64,256)
+            self.model.layer4[1]=MGCA_SE(512,64,8,64,512)
+            self.model.layer3[1]=MGCA_SE(256,64,8,64,256)
+            self.model.fc = torch.nn.Linear(in_features=512, out_features=num_classes)
             
         elif backbone == 'res50':
             self.model = resnet50()
-            model.layer4[1] = MGCA_SE(2048,128,64,128,2048)
-            model.layer4[2] = MGCA_SE(2048,128,64,128,2048)
-            model.layer3[1] = MGCA_SE(1024,64,64,64,1024)
-            model.layer3[2] = MGCA_SE(1024,64,64,64,1024)
-            model.layer3[3] = MGCA_SE(1024,64,64,64,1024)
-            model.layer3[4] = MGCA_SE(1024,64,64,64,1024)
-            model.layer3[5] = MGCA_SE(1024,64,64,64,1024)
+            self.model.layer4[1] = MGCA_SE(2048,128,64,128,2048)
+            self.model.layer4[2] = MGCA_SE(2048,128,64,128,2048)
+            self.model.layer3[1] = MGCA_SE(1024,64,64,64,1024)
+            self.model.layer3[2] = MGCA_SE(1024,64,64,64,1024)
+            self.model.layer3[3] = MGCA_SE(1024,64,64,64,1024)
+            self.model.layer3[4] = MGCA_SE(1024,64,64,64,1024)
+            self.model.layer3[5] = MGCA_SE(1024,64,64,64,1024)
+            self.model.fc = torch.nn.Linear(in_features=2048, out_features=num_classes)
         else:
             raise ValueError('Invalid input for network. Use "res18" or "res50".')
 
