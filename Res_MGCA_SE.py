19a85e17af4ea39e384c764ccfde5e1016d26acb
Sina Soleimani Fard (Sina SFard) <91623372+sinasfard@users.noreply.github.com>
2024-10-29 02:30:14 -0600

    Update Res_MGCA_SE.py

--- Res_MGCA_SE.py
+++ Res_MGCA_SE.py
@@ -9,18 +9,18 @@ class Res_MGCA_SE(nn.Module):
 
         if backbone == 'res18':
             self.model = resnet18()
-            model.layer4[1]=MHCA_resnet_V2(512,64,8,64,512)
-            model.layer3[1]=MHCA_resnet_V2(256,64,8,64,256)
+            model.layer4[1]=MGCA_SE(512,64,8,64,512)
+            model.layer3[1]=MGCA_SE(256,64,8,64,256)
             
         elif backbone == 'res50':
             self.model = resnet50()
-            model.layer4[1] = MHCA_resnet_V2(2048,128,64,128,2048)
-            model.layer4[2] = MHCA_resnet_V2(2048,128,64,128,2048)
-            model.layer3[1] = MHCA_resnet_V2(1024,64,64,64,1024)
-            model.layer3[2] = MHCA_resnet_V2(1024,64,64,64,1024)
-            model.layer3[3] = MHCA_resnet_V2(1024,64,64,64,1024)
-            model.layer3[4] = MHCA_resnet_V2(1024,64,64,64,1024)
-            model.layer3[5] = MHCA_resnet_V2(1024,64,64,64,1024)
+            model.layer4[1] = MGCA_SE(2048,128,64,128,2048)
+            model.layer4[2] = MGCA_SE(2048,128,64,128,2048)
+            model.layer3[1] = MGCA_SE(1024,64,64,64,1024)
+            model.layer3[2] = MGCA_SE(1024,64,64,64,1024)
+            model.layer3[3] = MGCA_SE(1024,64,64,64,1024)
+            model.layer3[4] = MGCA_SE(1024,64,64,64,1024)
+            model.layer3[5] = MGCA_SE(1024,64,64,64,1024)
         else:
             raise ValueError('Invalid input for network. Use "res18" or "res50".')
 
